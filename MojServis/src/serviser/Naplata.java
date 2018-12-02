cae213c2fb3c164803cac3ee0015c518f1133a0a
Djurdja5 <44476358+Djurdja5@users.noreply.github.com>
2018-12-02 17:39:14 +0100

    Add files via upload

--- MojServis/src/serviser/Naplata.java
+++ MojServis/src/serviser/Naplata.java
@@ -19,57 +19,7 @@ public class Naplata {
 	
 
 	
-	// METODA ZA SKRACENJE KODA SVAKE DRUGE METODE KOJA PROVERAVA PREKO EVIDENCIONOG BROJA
-	public static EvidencijaRacunara pretragaPoEvidencionomBroju() {
-		
-		System.out.println("Unesite evidencioni broj: ");
-		String eBroj = new Scanner(System.in).nextLine();
-		boolean objekatNePostoji = true;
-		EvidencijaRacunara e = null;
-		Serviser s = null;
-		
-		for(int i = 0; i < s.getListaRacunara().size(); i++ ) {
-			if (s.getListaRacunara().get(i).getEvidencioniBroj().equals(eBroj)) {
-				objekatNePostoji = false;
-				//return s.getListaRacunara().get(i); //****
-				e = s.getListaRacunara().get(i);
-				return e;
-			}
-		}if(objekatNePostoji == true)
-			System.err.println("Racunar sa ovim evidencionim brojem ne postoji na servisu! Pokusajte ponovo!");
-		
-		
-		return null;  //****
-		
-	}
-	
-	
-	// ODLUKA 3 - PLACANJE SERVISA 
-	public static void placanjeServisa() {
-		
-	}
-	
-	
-	// ODLUKA 5 - PRIKAZ ZARADE - KOLIKO JE PLACENO I KOLIKO JE ZA NAPLATU
-	public static void zarada () {
-		Long placeno = 0L; 
-		Long nijePlaceno = 0L;
-		Serviser s = null;
-		
-		
-		for(int i = 0; i < s.getListaRacunara().size(); i++) {
-			if(s.getListaRacunara().get(i).getNaplataServisa().getPlaceno() == true) {
-				placeno = placeno + s.getListaRacunara().get(i).getNaplataServisa().getIznos();
-			}else {
-				nijePlaceno = nijePlaceno + s.getListaRacunara().get(i).getNaplataServisa().getIznos();
-			}
-		}
-		
-		System.out.println("Placeno do sad: " + placeno);
-		System.out.println("Za naplatu: " + nijePlaceno);
-	}
-	
-	
+
 	
 //get//set	
 	public Long getIznos() {
